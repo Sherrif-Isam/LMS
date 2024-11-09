371820676723abd96e39341e9fb366b9ce3da918
Sherrif Isam <sherrifisam@yahoo.co.uk>
2024-11-09 00:30:43 +0000

    send models to admin

--- project/lms_app/models.py
+++ project/lms_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length = 50)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     status_book = [
@@ -24,3 +26,6 @@ class Book(models.Model):
     active = models.BooleanField(default = True)
     status = models.CharField(max_length=50, choices = status_book, null = True, blank = True)
     category = models.ForeignKey(Category, on_delete = models.PROTECT, null = True, blank = True)
+
+    def __str__(self):
+        return self.title
