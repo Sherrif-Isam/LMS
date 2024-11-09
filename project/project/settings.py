4a6294187e179491962ac7602e14f50f4020ab88
Sherrif Isam <sherrifisam@yahoo.co.uk>
2024-11-09 01:37:56 +0000

    media files

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,15 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'project/static'),
 ]
 
 #media files
 
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/' 
 
 
 # Default primary key field type
