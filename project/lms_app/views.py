f9c5969eec11db7ced442b6f11e0b433f649ee96
Sherrif Isam <sherrifisam@yahoo.co.uk>
2024-11-13 12:19:45 +0000

    imports the models into the views

--- project/lms_app/views.py
+++ project/lms_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
 def index(request):
-    return render(request,'pages/index.html')
+    contex ={
+        'book': Book.objects.all(),
+
+    }
+    return render(request,'pages/index.html', context)
 
 def books(request): 
     return render(request,'pages/books.html')
